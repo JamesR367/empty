45fcc6d18b198ce85ccfc1ace62177c70e3f942a
James Ramsey <77944661+JamesR367@users.noreply.github.com>
2025-11-11 15:08:21 +0000

    init commit

---
+++ AtmServices.Test/AtmTests.cs
@@ -0,0 +1,77 @@
+namespace AtmServices.Test;
+
+using AtmServices;
+
+public class AtmTests
+
+{
+
+    Atm testAtm;
+
+    int initialBalance = 100;
+
+    public AtmTests() {
+
+        testAtm = new Atm(initialBalance);
+
+    }
+
+ 
+
+    [Fact]
+
+    public void Test_Withdraw()
+
+    {
+
+        var result = testAtm.withdraw(25);
+
+        Assert.True(result);
+
+        Assert.Equal(75, testAtm.getBalance());
+
+    }
+
+    [Fact]
+
+    public void Test_Withdraw_Fail()
+
+    {
+
+        var result = testAtm.withdraw(125);
+
+        Assert.False(result);
+
+        Assert.Equal(100, testAtm.getBalance());
+
+    }
+
+    [Fact]
+
+    public void Test_Deposit()
+
+    {
+
+        var result = testAtm.deposit(125);
+
+        Assert.True(result);
+
+        Assert.Equal(225, testAtm.getBalance());
+
+    }
+
+    [Fact]
+
+    public void Test_Deposit_Fail()
+
+    {
+
+        var result = testAtm.deposit(-14);
+
+        Assert.False(result);
+
+        Assert.Equal(100, testAtm.getBalance());
+
+    }
+
+}
